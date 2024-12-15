9ae79cdd00c5766b21ae927d2d649d279e28bdfc
rehyy5@seznam.cz <rehyy5@seznam.cz>
2024-12-15 18:28:31 +0100

    App

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import {Inter, Luckiest_Guy, Lusitana, Roboto} from 'next/font/google';
+
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const roboto = Roboto({ weight: "300",subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight:"400" ,subsets:['latin']});
